cd0b24cb4f1ad1b469cb7d5c44141a942338442b
Dylan R <chemicalstudios@gmail.com>
2014-10-10 17:42:04 -0400

    changed bg color

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -21,9 +21,6 @@ public class GameScreen implements Screen {
 		PLAYING, PAUSE, OVER;
 	}
 	
-	private int score;
-	private int length;
-	
 	public GameScreen(SnakeGame game) {
 		this.game = game;
 	}
@@ -40,7 +37,7 @@ public class GameScreen implements Screen {
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 1, 1, 0);
+		Gdx.gl.glClearColor(0.2f, 0.5f, 0.2f, 0);
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
