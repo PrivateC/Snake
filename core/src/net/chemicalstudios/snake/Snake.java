4b6f72b2769abda22a676900501586cb90e5567d
Dylan R <chemicalstudios@gmail.com>
2014-10-11 21:05:40 -0400

    added little +1 that pops up after you eat, fixed world wrap glitch that occurred by going up or to the right, added game over screen

--- core/src/net/chemicalstudios/snake/Snake.java
+++ core/src/net/chemicalstudios/snake/Snake.java
@@ -10,74 +10,73 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 
 public class Snake implements InputProcessor {
 	ArrayList<Sprite> body;
-	
+
 	private Texture bodyTexture;
 
 	int startLength = 5;
-	
+
 	enum Direction {
 		UP, DOWN, LEFT, RIGHT;
 	}
-	
+
+	boolean movedThisFrame = false;
+
 	Direction currentDirection = Direction.RIGHT;
-	
+
 	private int moveAmount;
-	
+
 	GameScreen game;
-	
+
 	public Snake(GameScreen game) {
 		this.game = game;
-		body = new ArrayList<Sprite>();
-		
 		bodyTexture = new Texture("body.png");
+		reset();
+	}
+
+	public void update(Food food) {
 		
-		for(int i = 0; i < startLength; i++){
-			body.add(new Sprite(bodyTexture));
-			body.get(i).setPosition(Gdx.graphics.getWidth() / 2- ((i) * bodyTexture.getWidth()), Gdx.graphics.getHeight() / 2);
+		if (Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT)) {
+			if (currentDirection != Direction.RIGHT && !movedThisFrame) {
+				currentDirection = Direction.LEFT;
+			}
+		} else if (Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			if (currentDirection != Direction.LEFT && !movedThisFrame) {
+				currentDirection = Direction.RIGHT;
+			}
+		} else if (Gdx.input.isKeyPressed(Keys.S) || Gdx.input.isKeyPressed(Keys.DOWN)) {
+			if (currentDirection != Direction.UP && !movedThisFrame) {
+				currentDirection = Direction.DOWN;
+			}
+		} else if (Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP)) {
+			if (currentDirection != Direction.DOWN && !movedThisFrame) {
+				currentDirection = Direction.UP;
+			}
 		}
 		
-		moveAmount = bodyTexture.getWidth();
-		
-		body.add(new Sprite(bodyTexture));
-	}
-	
-	public void update(Food food) {
 		for (int i = body.size() - 1; i > 0; i--) {
 			body.get(i).setPosition(body.get(i-1).getX(), body.get(i - 1).getY());
-			
+
 			if (body.get(0).getX() < body.get(i).getX() + body.get(i).getWidth() && body.get(0).getX() + body.get(0).getWidth() > body.get(i).getX()) {
 				if (body.get(0).getY() < body.get(i).getY() + body.get(i).getHeight() && body.get(0).getY() + body.get(0).getHeight() > body.get(i).getY()) {
 					if (i > 1) {
-						game.setState(GameState.PAUSE);
+						game.setState(GameState.OVER);
 					}
 				}
 			}
 		}		
 		if (body.get(0).getX() < food.getX() + food.getWidth() && body.get(0).getX() + body.get(0).getWidth() > food.getX()) {
 			if (body.get(0).getY() < food.getY() + food.getHeight() && body.get(0).getY() + body.get(0).getHeight() > food.getY()) {
+				game.setScoreFade(true, new Vector2(food.getX(), food.getY()));
 				food.generate(body);
 				grow(1);
 			}
 		}
-		
-		if (body.get(0).getX() + body.get(0).getWidth() > Gdx.graphics.getWidth()) {
-			body.get(0).setX(0);
-			currentDirection = Direction.RIGHT;
-		} else if (body.get(0).getX() < 0) {
-			body.get(0).setX(Gdx.graphics.getWidth());
-			currentDirection = Direction.LEFT;
-		} else if (body.get(0).getY() < 0) {
-			body.get(0).setY(Gdx.graphics.getHeight());
-			currentDirection = Direction.DOWN;
-		} else if (body.get(0).getY() > Gdx.graphics.getHeight()) {
-			body.get(0).setY(0);
-			currentDirection = Direction.UP;
-		}
-		
+
 		switch (currentDirection) {
 		case UP:
 			body.get(0).translateY(moveAmount);
@@ -92,14 +91,30 @@ public class Snake implements InputProcessor {
 			body.get(0).translateX(moveAmount);
 			break;
 		}
+
+		if (body.get(0).getX() + body.get(0).getWidth() > Gdx.graphics.getWidth()) {
+			body.get(0).setX(0);
+			currentDirection = Direction.RIGHT;
+		} else if (body.get(0).getX() < 0) {
+			body.get(0).setX(Gdx.graphics.getWidth());
+			currentDirection = Direction.LEFT;
+		} else if (body.get(0).getY() < 0) {
+			body.get(0).setY(Gdx.graphics.getHeight());
+			currentDirection = Direction.DOWN;
+		} else if (body.get(0).getY() > Gdx.graphics.getHeight()) {
+			body.get(0).setY(0);
+			currentDirection = Direction.UP;
+		}
+
 		
+		movedThisFrame = false;
 	}
-	
+
 	public void grow(int amount) {
 		for (int i = 0; i < amount; i++) {
 			body.add(new Sprite(bodyTexture));
 			body.get(body.size() - 1).setPosition(body.get(body.size() - 2).getX(), body.get(body.size() - 2).getY());
-			
+
 			switch (currentDirection) {
 			case UP:
 				body.get(body.size() - 1).translateY(-moveAmount);
@@ -116,45 +131,33 @@ public class Snake implements InputProcessor {
 			}
 		}
 	}
-	
+
 	public ArrayList<Sprite> getSprites() {
 		return body;
 	}
 
 	@Override
 	public boolean keyDown(int keycode) {
-		switch (keycode) {
-		case Keys.A:
-		case Keys.LEFT:
-			if (currentDirection != Direction.RIGHT) currentDirection = Direction.LEFT;
-			break;
-		case Keys.D:
-		case Keys.RIGHT:
-			if (currentDirection != Direction.LEFT) currentDirection = Direction.RIGHT;
-			break;
-		case Keys.W:
-		case Keys.UP:
-			if (currentDirection != Direction.DOWN) currentDirection = Direction.UP;
-			break;
-		case Keys.S:
-		case Keys.DOWN:
-			if (currentDirection != Direction.UP) currentDirection = Direction.DOWN;
-			break;
-		case Keys.P:
-		case Keys.SPACE:
-			if (game.getState().equals(GameState.PAUSE)) {
-				game.setState(GameState.PLAYING);
-			} else if (game.getState().equals(GameState.PLAYING)) {
-				game.setState(GameState.PAUSE);
-			}
-			break;
-		}
-		
 		return false;
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
+		if (keycode == Keys.P || keycode == Keys.SPACE) {
+			switch (game.getState()) {
+			case PAUSE:
+				game.setState(GameState.PLAYING);
+				break;
+			case PLAYING:
+				game.setState(GameState.PAUSE);
+				break;
+			case OVER:
+				game.setState(GameState.PLAYING);
+				game.reset();
+				break;
+			}
+			game.scoreCoordMove = true;
+		}
 		return false;
 	}
 
@@ -187,8 +190,23 @@ public class Snake implements InputProcessor {
 	public boolean scrolled(int amount) {
 		return false;
 	}
-	
+
 	public void dispose() {
 		bodyTexture.dispose();
+	}
+
+	public void reset() {
+		body = new ArrayList<Sprite>();
+
+		currentDirection = Direction.RIGHT;
+
+		for(int i = 0; i < startLength; i++){
+			body.add(new Sprite(bodyTexture));
+			body.get(i).setPosition(Gdx.graphics.getWidth() / 2- ((i) * bodyTexture.getWidth()), Gdx.graphics.getHeight() / 2);
+		}
+
+		moveAmount = bodyTexture.getWidth();
+
+		body.add(new Sprite(bodyTexture));
 	}
 }
--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -7,6 +7,7 @@ import net.chemicalstudios.snake.SnakeGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -40,6 +41,16 @@ public class GameScreen implements Screen {
 	private Vector2 scoreCoord;
 	String pauseMessage;
 	private Vector2 pauseMsgCoord;
+	String gameOverMsg;
+	private Vector2 gameOverCoord;
+	int timer = 0;
+	int score = 0;
+	
+	String resetMsg;
+	private Vector2 resetCoord;
+	
+	SpriteBatch fade;
+	Texture plusOne;
 	
 	@Override
 	public void show() {
@@ -58,10 +69,34 @@ public class GameScreen implements Screen {
 		pauseMessage = new String("Paused! P or Spacebar to unpause!");
 		
 		pauseMsgCoord = new Vector2(Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(pauseMessage).width / 2), -100);
+		
+		gameOverMsg = new String("Game Over! Your final score is");
+		
+		gameOverCoord = new Vector2(Gdx.graphics.getWidth() * -1, Gdx.graphics.getHeight() / 2 - (bmFont.getCapHeight() / 2));
+		
+		resetMsg = new String("Spacebar to restart!");
+		resetCoord = new Vector2(Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(resetMsg).width / 2), gameOverCoord.y - (bmFont.getCapHeight()));
+		
+		/* Score fade effect */
+		fade = new SpriteBatch();
+		fadeLocation = new Vector2(0, 0);
+		fadeLocationEnd = new Vector2(0, 0);
+		plusOne = new Texture("plusOne.png");
 	}
 	
-	int timer = 0;
-	int score = 0;
+	public boolean scoreCoordMove = false;
+	
+	public boolean scoreFade = false;
+	private Vector2 fadeLocation;
+	private Vector2 fadeLocationEnd;
+	
+	public void setScoreFade(boolean n, Vector2 location) {
+		scoreFade = n;
+		fadeLocation.set(location);
+		fadeLocationEnd.set(fadeLocation.x, fadeLocation.y + 50);
+	}
+	
+	double alpha = 1.0;
 	
 	@Override
 	public void render(float delta) {
@@ -69,18 +104,36 @@ public class GameScreen implements Screen {
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
-		
 		if (currentState.equals(GameState.PLAYING)) {
-			if (scoreCoord.y >  bmFont.getCapHeight() * 2) {
-				scoreCoord.y -= (scoreCoord.y -  bmFont.getCapHeight() * 2) * 0.1;
-			} else {
-				scoreCoord.y =  bmFont.getCapHeight() * 2;
+			/* Smoothly transition the text */
+			if (scoreCoordMove) {
+				if (scoreCoord.y > bmFont.getCapHeight() * 2) {
+					scoreCoord.y -= (scoreCoord.y -  bmFont.getCapHeight() * 2) * 0.1;
+				} else {
+					scoreCoord.y =  bmFont.getCapHeight() * 2;
+				}
+				if (scoreCoord.x > (Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(String.valueOf(score)).width))) {
+					scoreCoord.x -= (scoreCoord.x - (Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(String.valueOf(score)).width))) * 0.1; 
+				} 
+				if ((int) (scoreCoord.x) == (int) ((Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(String.valueOf(score)).width))) &&
+						Math.round(scoreCoord.y) == Math.round(bmFont.getCapHeight() * 2)) {
+					scoreCoordMove = false;
+				}
 			}
+			
 			if (pauseMsgCoord.y > -100) {
 				pauseMsgCoord.y -= (pauseMsgCoord.y - -100) * 0.1;
 			} else {
 				pauseMsgCoord.y = -100;
 			}
+			
+			if (gameOverCoord.x > Gdx.graphics.getWidth() * -1) {
+				gameOverCoord.x -= (gameOverCoord.x - Gdx.graphics.getWidth() * -1) * 0.1;
+			}
+			if (resetCoord.x > Gdx.graphics.getWidth() * -1) {
+				resetCoord.x -= (resetCoord.x - Gdx.graphics.getWidth() * -1) * 0.1;
+			}
+			
 			
 			batch.draw(food.getTexture(), food.getX(), food.getY());
 			for (int i = 0; i < snake.getSprites().size(); i++) {
@@ -95,10 +148,11 @@ public class GameScreen implements Screen {
 			
 			bmFont.draw(batch, String.valueOf(score), scoreCoord.x, scoreCoord.y);
 			bmFont.draw(batch, pauseMessage, pauseMsgCoord.x, pauseMsgCoord.y);
-
+			bmFont.draw(batch, gameOverMsg, gameOverCoord.x, gameOverCoord.y);
+			
 			timer++;
 		} else if (currentState.equals(GameState.PAUSE)) {
-			
+			/* Smoothly transition the text */
 			if (scoreCoord.y < Gdx.graphics.getHeight() / 2 + (bmFont.getCapHeight() * 4)) {
 				scoreCoord.y += (Gdx.graphics.getHeight() / 2 + (bmFont.getCapHeight() * 4) - scoreCoord.y) * 0.1;
 			} else {
@@ -113,9 +167,54 @@ public class GameScreen implements Screen {
 			bmFont.draw(batch, pauseMessage, pauseMsgCoord.x, pauseMsgCoord.y);
 			bmFont.draw(batch, String.valueOf(score), scoreCoord.x, scoreCoord.y);
 		} else if (currentState.equals(GameState.OVER)) {
+			/* Smoothly transition the text */
+			if (gameOverCoord.x < Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(gameOverMsg + score).width / 2)) {
+				gameOverCoord.x += (Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(gameOverMsg + score).width / 2) - gameOverCoord.x) * 0.1;
+			}
+			if (resetCoord.x < Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(resetMsg).width / 2)) {
+				resetCoord.x += (Gdx.graphics.getWidth() / 2 - (bmFont.getBounds(resetMsg).width / 2) - resetCoord.x) * 0.1;
+			}
 			
+			if (scoreCoord.y < gameOverCoord.y) {
+				scoreCoord.y += (gameOverCoord.y - scoreCoord.y) * 0.1;
+			} else {
+				scoreCoord.y = gameOverCoord.y;
+			}
+			if (scoreCoord.x < (gameOverCoord.x + bmFont.getBounds(gameOverMsg + score).width)) {
+				scoreCoord.x += ((gameOverCoord.x + bmFont.getBounds(gameOverMsg + score).width) - scoreCoord.x)  * 0.1;
+			} else {
+				scoreCoord.x = gameOverCoord.x + bmFont.getBounds(gameOverMsg + score).width;
+			}
+			
+			bmFont.draw(batch, resetMsg, resetCoord.x, resetCoord.y);
+			bmFont.draw(batch, String.valueOf(score), scoreCoord.x, scoreCoord.y);
+			bmFont.draw(batch, gameOverMsg, gameOverCoord.x, gameOverCoord.y);
 		}
 		batch.end();
+		if (scoreFade && currentState.equals(GameState.PLAYING)) {
+			
+			fade.begin();
+			fade.setColor(1, 1, 1, (float) alpha);
+			
+			fade.draw(plusOne, fadeLocation.x, fadeLocation.y);
+			
+			fade.end();
+			
+			if (fadeLocation.y < fadeLocationEnd.y) {
+				fadeLocation.y += 1;
+				if (alpha > 0.03) {
+					alpha -= 0.03;
+				} else {
+					alpha = 0;
+				}
+			} else {
+				alpha = 1.0;
+				scoreFade = false;
+			}
+			
+			
+		}
+		
 	}
 
 	public void setState(GameState newState) {
@@ -133,7 +232,6 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void hide() {
-		
 	}
 
 	@Override
@@ -148,10 +246,14 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		bmFont.dispose();
-		ftfGen.dispose();
-		snake.dispose();
-		food.dispose();
+	}
+	
+	public void reset() {
+		timer = 0;
+		score = 0;
+	
+		scoreCoordMove = true;
+		
+		snake.reset();
 	}
 }
